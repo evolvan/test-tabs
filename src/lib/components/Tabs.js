e53e4c143e9dd31964adf576141cbb303c946428
Ramanjit <ramanjits@evolvan.com>
2022-03-30 13:09:52 +0530

    Fix - mobile responsive V:1.2.3

--- src/lib/components/Tabs.js
+++ src/lib/components/Tabs.js
@@ -19,9 +19,18 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
    const [currentTab,SetCurrentTab] = useState(0);
    const [data,SetData] = useState(props.data)
    const [currentData,SetCurrentData] = useState([])
+   const [currentTabs,SetCurrentTabs] = useState(0);
 
-   function handleClick(currentTab) {
-       SetCurrentTab(currentTab);
+   function handleClick(value) {
+        SetCurrentTab(value);  
+   }
+
+   function handleClicks(val) {
+       if(val === currentTabs) {
+        SetCurrentTabs(-1);
+       }else{
+        SetCurrentTabs(val);
+       }
    }
 
    function filterData(){
@@ -34,10 +43,10 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
    useEffect(()=>{
     filterData();
-   },[currentTab]);
+   },[currentTab, currentTabs]);
    
    return (
-       
+    <>
         <div className="tabs-main">
             <div className="tab">
             {
@@ -87,6 +96,58 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
                 }
             </div>
         </div>
+
+         {/* Mobile Only */}
+         
+             <div className="tabs-main mobile">
+                <div className="tab">
+                {
+                data &&
+                data.map((button, i) => {
+
+                    const currentClass = button?.index === currentTabs ? ' tablinks active' : 'tablinks';
+                    const currentIcon =  currentClass  === ' tablinks active' ? faMinus : faPlus;
+                    return (
+                        <>
+                            <button type="button" key={button?.id} className={currentClass} onClick={() => handleClicks(button?.index)}>
+                                <img src={button.icon} className="icon-img" alt="Image not found" />
+                                <span> {button?.title} </span>
+                                <FontAwesomeIcon icon={currentIcon} />
+                            </button>
+                            
+                            <div className={`tabcontent ${button?.index === currentTabs ? ' open' : 'close'}`}>
+                                <>
+                                    <div className="tab-content-inner">
+                                        <h3 className="content-title text-center">{data[i]?.title}</h3>
+                                        <div className="content-sub-heading text-center">{data[i]?.subTitle}</div>
+
+                                        <p className="spacer">&nbsp;</p>
+                                        <div className="mobi-flex">
+                                        <div className="content-body" dangerouslySetInnerHTML={{__html:`<img src=${data[i]?.img} class="rounded-img" alt="Image not found"/>`+data[i]?.body}}></div>
+                                        </div>
+                                        <div className="content-links-container">
+                                            <div className="links">
+                                            {data[i]?.link1 &&
+                                                <a href={data[i]?.link1} className="links-btn link-1" target="_blank">Learn More</a>
+                                            }
+                                            {data[i]?.link2 &&
+                                                <a href={data[i]?.link2} className="links-btn link-2" target="_blank">Free Evaluation</a>
+                                            }
+                                            </div>
+                                        </div>
+                                    </div>
+                                </>
+                                : 
+                               
+                            </div>
+
+                        </>
+                    ) 
+                })
+                }
+                </div>
+            </div>
+         </>
 
     )
 }
